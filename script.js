ad88a074816327c21eccaaa0cb36644e3d3760ee
FaiqaAbbas <faiqaabbas474@gmail.com>
2024-04-10 12:05:16 +0500

    Updates css

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const scroll = new LocomotiveScroll({
-        el: document.querySelector("#wrapper"), // Changed selector to #wrapper
-        smooth: true
+        el: document.querySelector("#wrapper"),
+        smooth: true,
+
+      });
     });
+<<<<<<< Updated upstream
     
 });
+=======
+
+function x(y){
+  console.log('x');
+  y();
+  console.log("hello Y is gone")
+}
+setTimeout(function y(){
+  console.log('y');
+},1000);
+>>>>>>> Stashed changes
