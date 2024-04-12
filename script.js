0a28478d6623093bc89098f25c49c2fced886cf0
FaiqaAbbas <134505855+FaiqaAbbas@users.noreply.github.com>
2024-04-12 23:44:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 document.addEventListener("DOMContentLoaded", function() {
     const scroll = new LocomotiveScroll({
         el: document.querySelector("#wrapper"),
@@ -7,58 +6,3 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
 
-=======
->>>>>>> Stashed changes
-
-function x(y){
-  console.log('x');
-  y();
-  console.log("after y")
-}
-<<<<<<< Updated upstream
-setTimeout(function y(){
-  console.log('y');
-},1000);
-=======
-
-x(function y(){
-    setTimeout(function(){
-      for(i=0;i<20;i++){
-        console.log(i);
-      }
-    },2000)
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const scroll = new LocomotiveScroll({
-//         el: document.querySelector("#wrapper"),
-//         smooth: true,
-
-//       });
-//     });
->>>>>>> Stashed changes
