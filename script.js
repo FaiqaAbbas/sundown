1668c16a53dfee4fd0f9ecdc22044d62231359dd
FaiqaAbbas <faiqaabbas474@gmail.com>
2024-04-27 21:56:06 +0500

    hover affect picture or video for projects

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//  locomotive library
 document.addEventListener("DOMContentLoaded", function() {
     const scroll = new LocomotiveScroll({
         el: document.querySelector("#wrapper"),
@@ -6,3 +7,25 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
 
+var project_video=document.querySelector(".video-on-hover");
+var project_list=document.querySelector(".featured-projects-list");
+var project=document.querySelectorAll(".featured-projects-list .project");
+project.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+
+      project_video.style.display="block";
+      project_video.style.background=`url(${ e.getAttribute('data-src')})`
+
+    })
+    e.addEventListener("mouseleave",function(){
+      project_video.style.display="none";
+    })
+})
+// // var data_source=project.getAttribute('data_src');
+// // alert(data_source);
+// project_list.addEventListener("mouseenter",function(){
+//   project_video.style.display="block";
+// });
+// project_list.addEventListener("mouseleave",function(){
+//   project_video.style.display="none";
+// });
