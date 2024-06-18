296c25b19b485cdf56221cb06755e3d4cd5df08a
Faiqa <faiqaabbas474@gmail.com>
2024-06-18 14:43:11 +0500

    testing

--- carousel_container.js
+++
@@ -1,7 +0,0 @@
-
-const carousel_container=document.querySelector('.carousel-container')
-const cursor=document.querySelector('.cursor')
-carousel_container.addEventListener('mousemove',function(details){ 
-  cursor.style.top=`${details.clientY}px`; 
-  cursor.style.left=`${details.clientX}px`; 
-})
